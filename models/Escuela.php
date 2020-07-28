0206abaf7f7b18b805865f38f8486935911fe1d2
Alan Gonzalez <argaalan@hotmail.com>
2020-07-28 12:20:59 -0600

    New queries escuela

--- models/Escuela.php
+++ models/Escuela.php
@@ -28,7 +28,6 @@
       
     }
 
-
     // Update escuela 
     public function updateEscuela($idEscuela, $nombre, $numero, $turno, $idLocalidad) {
       $this->db->query('UPDATE Escuela 
@@ -52,4 +51,70 @@
       return false;
       
     }
+    
+    // Insert localidad
+    public function insertLocalidad($ciudad){
+        $query = 'INSERT INTO Localidad (idLocalidad, nombre) VALUES (null, :ciudad)';
+        
+        $this->db->query($query);
+        
+        $this->db->bind(':ciudad', $ciudad);
+        
+        if ($this->db->execute()) {
+            return true;
+        }
+        return false;
+    }
+    
+    // Insert escuela
+    public function insertEscuela($nombre,$numero,$turno,$localidad){
+        $query = 'INSERT INTO Escuela (idEscuela, nombre, numero, turno, idLocalidad) '
+                . 'VALUES (null, :nombreEsc, :numeroEsc, :turnoEsc, :idLoc)';
+        
+        $this->db->query($query);
+        
+        $this->db->bind(':nombreEsc', $nombre);
+        $this->db->bind(':numeroEsc', $numero);
+        $this->db->bind(':turnoEsc', $turno);
+        $this->db->bind(':idLoc', $localidad);
+        
+        if ($this->db->execute()) {
+            return true;
+        }
+        return false;
+    }
+    
+    // Single update escuela
+    public function singleUpdateEscuela($variable,$variableCambiar,$nombreEscuela){
+        $query = 'UPDATE Escuela 
+	SET :variable = :variableCambiar
+	WHERE nombre = :nombreEscuela';
+        
+        $this->db->query($query);
+        
+        $this->db->bind(':variable', $variable);
+        $this->db->bind(':variableCambiar', $variableCambiar);
+        $this->db->bind(':nombreEscuela', $nombreEscuela);
+        
+        if ($this->db->execute()) {
+            return true;
+        }
+        return false;
+    }
+    
+    // Delete escuela
+    public function deleteEscuela($nombreEscuela){
+        $this->db->query('DELETE FROM Escuela WHERE nombre = :nombreEscuela');
+        
+        // Bind value
+        $this->db->bind(':nombreEscuela', $nombreEscuela);
+        
+        // Execute
+        if ($this->db->execute()) {
+            return true;
+        }
+        return false;
+        
+    }
+    
   }
